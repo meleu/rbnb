413609468b182d9e144886c0c52c818bf83bdc91
Augusto Lopes <meleu.dev@gmail.com>
2023-07-06 12:49:52 -0300

    feat: add name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validations
+  ######################################################################
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
