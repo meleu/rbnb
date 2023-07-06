ce4a2a0a3502983106b35e709cf00e4dd1cc7dd8
Anne Bortoli <annebortoli@gmail.com>
2023-07-06 17:47:42 -0300

    :seedling: feat: Add seeds with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+User.destroy_all
+Property.destroy_all
+
+puts "🌱 Seeding data..."
+puts "👩🏽‍🦱 Creating users with faker..."
+
+10.times do
+  fn = Faker::Name.first_name
+  ln = Faker::Name.last_name
+
+  User.create(
+    name: "#{fn} #{ln}",
+    email: "#{fn.downcase}@email.com",
+    password: '123456'
+  )
+end
+
+puts "🏠 Creating properties with faker..."
+
+10.times do
+
+  Property.create(
+    title: Faker::Tea.variety,
+    description: Faker::Restaurant.description,
+    price: Faker::Commerce.price,
+    owner_id: rand(1..10)
+  )
+end
+
+puts 'Finished!'
