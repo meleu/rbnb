c2aae2a02a7b3cf13b6ba910a58a6b1e189a402b
Augusto Lopes <meleu.dev@gmail.com>
2023-07-06 15:32:52 -0300

    test: user and property model tests

--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -1,7 +1,21 @@
-require "test_helper"
+require 'test_helper'
 
 class PropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'cannot create property without a title' do
+    property = Property.new(
+      owner: users(:meleu)
+    )
+
+    refute property.save
+    assert_equal ["can't be blank"], property.errors[:title]
+  end
+
+  test 'cannot create property without an owner' do
+    property = Property.new(
+      title: 'Cool House'
+    )
+
+    refute property.save
+    assert_equal ['must exist'], property.errors[:owner]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'cannot create user without a name' do
+    user = user_no_name
+
+    refute user.save
+    assert_equal ["can't be blank"], user.errors[:name]
+  end
+
+  test 'cannot create user without an invalid email' do
+    user = user_invalid_email
+
+    refute user.save
+    assert_equal ['is invalid'], user.errors[:email]
+  end
+
+  def user_no_name
+    User.new(
+      email: 'user@example.com',
+      password: '123456'
+    )
+  end
+
+  def user_invalid_email
+    User.create(
+      name: 'Anyname',
+      email: 'invalid-email',
+      password: '123456'
+    )
+  end
 end
