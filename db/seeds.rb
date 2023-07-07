4680f63ba13b5e510269933c2435b7b8e62fb7e9
Augusto Lopes <meleu.dev@gmail.com>
2023-07-07 17:07:39 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,76 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+User.destroy_all
+Property.destroy_all
+
+puts '🌱 Seeding data...'
+
+User.create!(
+  first_name: 'Ariel',
+  last_name: 'Lima',
+  email: 'ariel@email.com',
+  password: '123456'
+)
+
+User.create!(
+  first_name: 'Anne',
+  last_name: 'Bortoli',
+  email: 'anne@email.com',
+  password: '123456'
+)
+
+User.create!(
+  first_name: 'Augusto',
+  last_name: 'Lopes',
+  email: 'meleu@email.com',
+  password: '123456'
+)
+
+puts '👩🏽‍🦱 Creating users with faker...'
+
+10.times do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create!(
+    first_name:,
+    last_name:,
+    email: "#{first_name.downcase}_#{i}@email.com",
+    password: '123456'
+  )
+end
+
+puts '🏠 Creating properties with faker...'
+
+Property.create!(
+  title: Faker::Tea.variety,
+  description: Faker::Restaurant.description,
+  price: rand(100..200),
+  owner_id: 1
+)
+
+Property.create!(
+  title: Faker::Tea.variety,
+  description: Faker::Restaurant.description,
+  price: rand(100..200),
+  owner_id: 2
+)
+
+Property.create!(
+  title: Faker::Tea.variety,
+  description: Faker::Restaurant.description,
+  price: rand(100..200),
+  owner_id: 3
+)
+
+10.times do
+  Property.create!(
+    title: Faker::Tea.variety,
+    description: Faker::Restaurant.description,
+    price: rand(100..200),
+    owner: User.all.sample
+  )
+end
+
+puts 'Finished!'
